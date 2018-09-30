ae260a4fb688e017c035b8511e9c3225d4b6da6d
maurilioHH <mauriliostizzo@gmail.com>
2018-09-30 21:08:44 +0200

    -

--- todo/app/model.ts
+++ todo/app/model.ts
@@ -4,11 +4,13 @@ export class Model {
     constructor() {
         this.user = "Adam";
         this.items = [new TodoItem("Buy Flowers", false),
-        new TodoItem("Get Shoes", false),
-        new TodoItem("Collect Tickets", false),
-        new TodoItem("Call Joe", false)]
+                    new TodoItem("Get Shoes", false),
+                    new TodoItem("Collect Tickets", false),
+                    new TodoItem("Call Joe", false)
+        ]
     }
 }
+
 export class TodoItem {
     action;
     done;
